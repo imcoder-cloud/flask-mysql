7fab7b447600e5f07ffa743050a0747fdcaddf9f
dongdongchen <dd18355193335@163.com>
2019-11-28 15:12:48 +0800

    打印sql语句

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ class DevConfig:
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/test?charset=UTF8MB4"
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_ECHO = True # 打印sql语句
